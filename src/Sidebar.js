948c3208e4dbb36c6597a0c55479d206a9924048
Avinash.B <avinash.fullstack@outlook.com>
2020-10-10 23:34:39 +0530

    adding material ui icons

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,12 +1,14 @@
-import React from 'react'
-import './Sidebar.css'
+import React from 'react';
+import './Sidebar.css';
 
 function Sidebar() {
-    return (
-        <div className='sidebar'>
-            <h2>sidebar</h2>
-        </div>
-    )
+	return (
+		<div className="sidebar">
+			<div className="sidebar__top">
+				<h3>clever programmer</h3>
+			</div>
+		</div>
+	);
 }
 
-export default Sidebar
+export default Sidebar;
